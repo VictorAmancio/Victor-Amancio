44c3c4914136c2dd5b786c880d33bdf9d38863a0
Victor <victor_amancio@hotmail.com>
2023-02-24 11:14:11 -0300

    Realizado a criação do About

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute('src', '/assets/avatar.png')
+    img.setAttribute("src", "/assets/avatar.png")
   }
- 
 }
+
+window.sr = ScrollReveal({ reset: true })
+sr.reveal('.row', { duration: 2000 })
+sr.reveal('.row', {rotate: { x: 80, y:0, z:0 }, duration: 3000});
+
