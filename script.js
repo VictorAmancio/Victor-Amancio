d07f26df7df4c6270bc00456fbc6236f0a7abbe3
Victor <victor_amancio@hotmail.com>
2023-02-28 17:03:38 -0300

    Feito Ajuste do JS, mais informação no about

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   // substituir a imagem
-  if (html.classList.contains("light")) {
+  if (html.classList.contains('light')) {
     // se tiver light mode adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "/assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
 
 window.sr = ScrollReveal({ reset: true })
-sr.reveal('.row', { duration: 2000 })
-sr.reveal('.row', {rotate: { x: 80, y:0, z:0 }, duration: 3000});
+sr.reveal(".row", { duration: 2000 })
+sr.reveal(".row", { rotate: { x: 80, y: 0, z: 0 }, duration: 3000 })
 
